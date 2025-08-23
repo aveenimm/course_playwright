81fa202f2635a2cae7ddfd33b4c4f8e6cce6765d
aveenimm <aveenimm@mail.ru>
2025-08-23 23:29:57 +0300

    день 2

---
+++ lesson/playwright_hover.py
@@ -0,0 +1,14 @@
+#Если навести курсор мыши на эту ссылку, она будет подчёркнута  
+from playwright.sync_api import sync_playwright, expect
+
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
+
+    registration_link = page.get_by_test_id('login-page-registration-link')
+    registration_link.hover()
+
+    page.wait_for_timeout(5000)
