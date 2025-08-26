6554b955af522074e49c231cadb86d419089bda7
aveenimm <aveenimm@mail.ru>
2025-08-26 16:22:38 +0300

    day5

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,33 +1,23 @@
 import pytest
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Page, expect
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successfull_reg():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+def test_successfull_reg(chromium_page: Page):
 
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user.name@gmail.com')
+    email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
 
-        username_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('username')
+    username_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('username')
 
-        pass_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        pass_input.fill('password')
+    pass_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
+    pass_input.fill('password')
 
-        reg_button = page.get_by_test_id('registration-page-registration-button')
-        reg_button.click()
+    reg_button = chromium_page.get_by_test_id('registration-page-registration-button')
+    reg_button.click()
 
-        context.storage_state(path="browser-state.json")
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state='browser-state.json')
-        page = context.new_page()
-
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+    dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_title).to_be_visible()
